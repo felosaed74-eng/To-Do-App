34c20e8f417dde0066cc224b5f516b88fac8ac0e
Felo saed <felosaed74@gmail.com>
2026-09-25 13:21:01 +0300

    feat(create profile): finish create profile -build profile screen -build database profile -handling state of profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_app/core/app_routes.dart';
 import 'package:todo_app/view/screens/add_task_screen.dart';
 import 'package:todo_app/view/screens/home_screen.dart';
 import 'package:todo_app/view/screens/profile_screen.dart';
+import 'data/model/user_model.dart';
 
-void main(){
+void main() async{
+  await Hive.initFlutter();
+  WidgetsFlutterBinding.ensureInitialized();
+  Hive.registerAdapter(UserModelAdapter());
+  Hive.openBox<UserModel>('User');
+  
   runApp(const ToDoApp());
 }
 
--- lib/view/screens/profile_screen.dart
+++ lib/view/screens/profile_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todo_app/core/app_routes.dart';
+import 'package:todo_app/data/model/user_model.dart';
 import 'package:todo_app/view/widget/coustom_text_form_feild_widget.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -18,7 +22,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
         padding: const EdgeInsets.symmetric(horizontal: 24),
         child: Column(
           children: [
-            SizedBox(height: 100,),
+            SizedBox(height: 100),
             Container(
               padding: EdgeInsets.all(30),
               decoration: BoxDecoration(
@@ -46,7 +50,21 @@ class _ProfileScreenState extends State<ProfileScreen> {
             ),
             SizedBox(height: 50,),
             MaterialButton(
-              onPressed: (){}, 
+              onPressed: () async {
+                _showLoading();
+                var userBox = Hive.box<UserModel>('User');
+                await userBox
+                .put("UserKey", UserModel(fullName: fullName.text),)
+                .then((value) {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).pushNamed(AppRoutes.home);
+                })
+                .catchError((error){ 
+                  Navigator.of(context).pop();
+                  _showError(error);   
+                });
+                
+              }, 
               color: Color(0xff3F51B5),
               padding: EdgeInsets.all(10),
               minWidth: 300,
@@ -59,11 +77,60 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 fontSize: 20,
                 fontWeight: .bold,
                 color: Colors.white
-              ),))
+              ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
-}
+  Future<void> _showLoading() async{
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context){
+      return AlertDialog(
+        content: Row(
+          spacing: 20,
+          children: [
+            CircularProgressIndicator(),
+             Text("Loading....",
+             style: TextStyle(
+              fontSize: 16,
+              fontWeight: .w400,
+             ),),
+          ],
+        ) ,
+      );
+    } 
+  );
+  }
+  Future<void> _showError( String error) async{
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context){
+      return AlertDialog(
+        title: const Text(
+          "Error",
+          style: TextStyle(
+            fontSize: 20, 
+            fontWeight: .bold,
+            color: Colors.red
+            ),),
+        content: Text(error, style: TextStyle( fontSize: 16, fontWeight: .w600),),
+        actions: [
+          TextButton(
+            child: const Text("Okay"),
+            onPressed: (){
+              Navigator.of(context).pop();
+            },
+            )
+        ],
+      );
+    } 
+  );
+  }
 
+}
